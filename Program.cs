9e9f6e210cea4ae590e2bd7133388e8165259d56
Dev.Yen <hoyen204@gmail.com>
2023-10-16 19:56:10 +0700

    Init project

---
+++ Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+using ValorantBlood.Properties;
+
+namespace ValorantBlood
+{
+    public class Valorant_Blood
+    {
+        public static string VALORANT_DEFAULT_PATH = @"C:\Riot Games\VALORANT";
+        public static string VALORANT_PROCESS = "VALORANT.exe";
+        public static string PAKS_PATH = @"ShooterGame\Content\Paks";
+        public static string VNG_LOGO_PAK = Path.Combine(PAKS_PATH, "VNGLogo-WindowsClient.pak");
+        public static string VNG_LOGO_SIG = Path.Combine(PAKS_PATH, "VNGLogo-WindowsClient.sig");
+        public static string MATURE_PAK = Path.Combine(PAKS_PATH, "MatureData-WindowsClient.pak");
+        public static string MATURE_SIG = Path.Combine(PAKS_PATH, "MatureData-WindowsClient.sig");
+
+        public static void Main(string[] args)
+        {
+            Console.Write($"Select Valorant path ({VALORANT_DEFAULT_PATH}): ");
+            string input = Console.ReadLine() ?? string.Empty;
+            if (!string.IsNullOrEmpty(input))
+            {
+                VALORANT_DEFAULT_PATH = input;
+            }
+
+            var ValorantLivePath = Path.Combine(VALORANT_DEFAULT_PATH, "live");
+            var ValorantProcessPath = Path.Combine(ValorantLivePath, VALORANT_PROCESS);
+            var VNGLogoPak = Path.Combine(ValorantLivePath, VNG_LOGO_PAK);
+            var VNGLogoSig = Path.Combine(ValorantLivePath, VNG_LOGO_SIG);
+            var MaturePak = Path.Combine(ValorantLivePath, MATURE_PAK);
+            var MatureSig = Path.Combine(ValorantLivePath, MATURE_SIG);
+
+            if (!File.Exists(ValorantProcessPath))
+            {
+                Console.WriteLine($"Not found {ValorantProcessPath}");
+                Console.ReadLine();
+                return;
+            }
+
+            if (File.Exists(VNGLogoPak))
+                File.Delete(VNGLogoPak);
+            if (File.Exists(VNGLogoSig))
+                File.Delete(VNGLogoSig);
+
+            if (!File.Exists(MaturePak))
+                using (MemoryStream resourceStream = new MemoryStream(Resources.MatureDataPak))
+                {
+                    if (resourceStream != null)
+                    {
+                        using (FileStream fileStream = File.Create(MaturePak))
+                        {
+                            resourceStream.CopyTo(fileStream);
+                        }
+
+                        Console.WriteLine("File copied successfully!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Resource not found.");
+                    }
+                }
+
+            if (!File.Exists(MatureSig))
+                using (MemoryStream resourceStream = new MemoryStream(Resources.MatureDataSig))
+                {
+                    if (resourceStream != null)
+                    {
+                        using (FileStream fileStream = File.Create(MatureSig))
+                        {
+                            resourceStream.CopyTo(fileStream);
+                        }
+
+                        Console.WriteLine("File copied successfully!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Resource not found.");
+                    }
+                }
+
+            Process.Start(Path.Combine(VALORANT_DEFAULT_PATH.Replace("VALORANT", "Riot Client"),"RiotClientServices.exe"), " --launch-product=valorant --launch-patchline=live");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Valorant-Blood
+
 Help you start Valorant game with mature settings.
+
+## Prerequisites
+
+Before you can build and run this project, make sure you have the following prerequisites installed on your system:
+
+- [.NET 6.0 SDK](https://dotnet.microsoft.com/download/dotnet/6.0)
+
+## Getting Started
+
+Follow these steps to build and run the project:
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/yourusername/your-project.git
+   cd your-project
+   ```
+
+2. Build the project using the .NET CLI:
+
+   ```bash
+   dotnet build
+   ```
+
+3. Run the project:
+
+   ```bash
+   dotnet build
+   ```
+
+That's it! Your project should now be up and running.
